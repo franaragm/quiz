c4d7c63a6f099b78f9a7f4178086af5821d65731
franaragm <franaragonmesa@gmail.com>
2015-07-19 16:51:29 +0200

    Añadir preguntas y validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -34,7 +34,7 @@ exports.index = function(req, res, next){
     }
 
     models.Quiz.findAll(query).then(function(quizes){
-        res.render('quizes/index', {quizes: quizes});
+        res.render('quizes/index', {quizes: quizes, errors: []});
     }).catch(function(error){
         next(error);
     });
@@ -42,7 +42,7 @@ exports.index = function(req, res, next){
 
 // GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -51,5 +51,36 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
+
+// GET /quizes/new
+// El metodo build(..) de sequelize 
+// crea un objeto no persistente asociado a la tabla Quiz, con las propiedades inicializadas. 
+// Este objeto se utiliza aqui solo para renderizar las vistas.
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build( // crea objeto quiz 
+        {pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+    res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+
+    quiz.validate().then(function(err){
+        if(err){
+            res.render('quizes/new', {quiz: quiz, errors: err.errors});
+        }else{
+            // guarda en DB los campos pregunta y respuesta de quiz
+            quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+                res.redirect('/quizes');
+            }); // Redireccion HTTP (URL relativo) lista de preguntas
+        }
+    });
+};
+
+
+
+
